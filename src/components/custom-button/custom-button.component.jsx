b6fc37fd25b990478186bedae5a53de26d34eca3
Esquinazi <esquinazi.dani@gmail.com>
2020-09-19 11:47:36 -0300

    Add redux-logger only on dev builds

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -5,7 +5,7 @@ import './custom-button.styles.scss';
 function CustomButton({ children, inverted, isGoogleSignIn, ...otherProps }) {
   return (
     <button
-      className={`${inverted && 'inverted'}${
+      className={`${inverted && 'inverted'} ${
         isGoogleSignIn && 'google-sign-in'
       } custom-button`}
       {...otherProps}
